66e131a665e5cf1937462ace05c5c713acdf97da
tmoynandy <tmoy.nandy@gmail.com>
2019-04-08 13:59:23 +0530

    changed product schema. added validation

--- api/models/product.js
+++ api/models/product.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    name : String,
-    price : Number
+    name : {type : String, required : true},
+    price : {type : Number, required : true}
 });
 
 
