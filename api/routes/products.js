9dedf59af4df843700c56a176d7959e59814f9aa
tmoynandy <tmoy.nandy@gmail.com>
2019-04-08 12:52:00 +0530

    added Fetch for all products

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,15 +5,27 @@ const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) =>{
-    //for normal static check
-    res.status(200).json({
-        message : 'Handling GET request to /products'
+    //for fetching all stored data
+    // res.status(200).json({
+    //     message : 'Handling GET request to /products'
+    // });
+    Product.find().exec()
+    .then(docs =>{
+        console.log(docs);
+        res.status(200).json(docs);
+    })
+    .catch(err =>{
+        console.log(err);
+        res.status(500).json({
+            error : err
+        });
     });
+
 });
 
 router.get('/:productId', (req, res, next) =>{
     const id = req.params.productId;
-    // //for static data viewing for test
+    // //for static single data viewing for test
     // if (id === 'special'){
     //     res.status(200).json({
     //         message : 'special ID',
@@ -29,7 +41,14 @@ router.get('/:productId', (req, res, next) =>{
     .exec()
     .then(doc =>{
         console.log(doc);
-        res.status(200).json(doc);
+        if (doc){
+            res.status(200).json(doc);
+        }
+        else{
+            res.status(404).json({
+                message : 'No valid entry found'
+            })
+        }
     })
     .catch(err => {
         console.log(err);
@@ -56,21 +75,24 @@ router.delete('/:productId', (req, res, next) =>{
 });
 
 router.post('/', (req, res, next) =>{
-    const newProduct = {
-        name : req.body.name,
-        price : req.body.price
-    };
+   
     const product = new Product({
         name: req.body.name,
         price: req.body.price
     });
-    product.save().then(result =>{
+    product.save()
+    .then(result =>{
         console.log(result);
+        res.status(201).json({
+            message : 'product added',
+            createdProduct : result
+        })
     })
-    .catch(err => console.log(err));
-    res.status(201).json({
-        message : 'Handling POST request to /products',
-        newProduct : newProduct
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error : err
+        })
     });
 });
 
