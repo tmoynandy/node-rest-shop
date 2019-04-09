0f8235a044a964018b5328308fafe53237d9f5b8
tmoynandy <tmoy.nandy@gmail.com>
2019-04-10 03:35:20 +0530

    added File upload validation - size & type

--- api/routes/products.js
+++ api/routes/products.js
@@ -3,7 +3,31 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const multer = require('multer');
-const upload = multer({dest :'uploads'});
+
+const storage = multer.diskStorage({
+    destination : function (req, file, cb){
+        cb(null, './uploads');
+    },
+    filename : function (req, file, cb){
+        cb(null, new Date().toISOString()+file.originalname);
+    }
+});
+const fileFilter = (req, file, cb) =>{
+    if( file.mimeType === 'image/jpeg' || file.mimeType === 'image/png') {
+        cb(null, true);
+    }
+    else{
+        cb(null, false);
+    }
+};
+
+const upload = multer({
+    storage : storage, 
+    limits : {
+    fileSize : 1024 * 1024 * 5
+    },
+    fileFilter : fileFilter
+});
 
 const Product = require('../models/product');
 
