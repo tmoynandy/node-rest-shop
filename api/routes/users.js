54cc84d8550278b71d835fc6b6f595708a4c3d3e
tmoynandy <tmoy.nandy@gmail.com>
2019-04-12 18:00:59 +0530

    Added user Login-without JWT

--- api/routes/users.js
+++ api/routes/users.js
@@ -68,6 +68,36 @@ router.delete('/:userId', (req, res, next) =>{
         })
     })
 
+});
+
+router.post('/login', (req,res,next) =>{
+    User.find({
+        email : req.body.email
+    }).exec()
+    .then( user =>{
+        if(user.length<1){
+            return res.status(404).json({
+                message : 'Auth Failed'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
+            if(err){
+                return res.status(401).json({
+                    message : 'Auth failed'
+                });
+            }
+            if(result){
+                return res.status(200).json({
+                    message : 'Auth Successful'
+                });
+            }
+            return res.status(401).json({
+                message : 'Auth failed'
+            });
+
+        })
+    })
+    .catch();
 })
 
 module.exports = router
