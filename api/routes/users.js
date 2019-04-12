690dcec2fa382781c9f1c1d8f21421bad5fcb873
tmoynandy <tmoy.nandy@gmail.com>
2019-04-12 17:38:24 +0530

    Added Delete users

--- api/routes/users.js
+++ api/routes/users.js
@@ -49,4 +49,21 @@ router.post('/signup', (req, res, next) => {
     
 });
 
+router.delete('/:userId', (req, res, next) =>{
+    User.remove({
+        _id : req.params.id
+    }).exec()
+    .then( result =>{
+        res.status(200).json({
+            message : 'User Deleted'
+        });
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error : err
+        })
+    })
+
+})
+
 module.exports = router
