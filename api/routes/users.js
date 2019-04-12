87c691269070eb7c72d3c3f44013a9fcf8ef7106
tmoynandy <tmoy.nandy@gmail.com>
2019-04-12 18:24:16 +0530

    Added JWT to Login

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+
+const secret = 'secret'
 
 router.post('/signup', (req, res, next) => {
     User.find({
@@ -87,12 +90,21 @@ router.post('/login', (req,res,next) =>{
                 });
             }
             if(result){
+                const token = jwt.sign({
+                    email : user[0].email,
+                    userId : user[0]._id
+                }, secret, 
+                {
+                    expiresIn : "1h"
+                }
+                )
                 return res.status(200).json({
-                    message : 'Auth Successful'
+                    message : 'Auth Successful',
+                    token : token
                 });
             }
             return res.status(401).json({
-                message : 'Auth failed'
+                message : 'Auth failed',
             });
 
         })
