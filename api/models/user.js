f191b61348c1a84380977dc0aa83c75920fe24e9
tmoynandy <tmoy.nandy@gmail.com>
2019-04-12 17:39:33 +0530

    changed user schema - email-unique

--- api/models/user.js
+++ api/models/user.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     name : {type : String, required : true},
-    email : {type : String, required : true},
+    email : {type : String, required : true, unique : true},
     password : {type : String, required : true}
 });
 
