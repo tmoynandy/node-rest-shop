06e1c47405d7d0f65a98b7df509df01d4a792977
tmoynandy <tmoy.nandy@gmail.com>
2019-04-13 00:30:43 +0530

    added Users controller

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,110 +6,14 @@ const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
+const UsersController = require('../controllers/users');
+
 const secret = 'secret'
 
-router.post('/signup', (req, res, next) => {
-    User.find({
-        email : req.body.email
-    }).exec()
-    .then( user =>{
-        if(user.length >=1){
-            res.status(409).json({
-                message : "Email already exists"
-            });
-        }
-        else{
-            bcrypt.hash(req.body.password, 10, (err, hash) =>{
-                if(err){
-                    return res.status(500).json({
-                        error : err
-                    });
-                }
-                else{
-                    const user = new User({
-                        name: req.body.name,
-                        email: req.body.email,
-                        password : hash
-                    });
-                    user.save()
-                    .then( result => {
-                        console.log(result);
-                        res.status(201).json({
-                            message : 'user created'
-                        });
-                    })
-                    .catch( err =>{
-                        res.status(500).json({
-                            error : err
-                        });
-                    });
-                }
-            });
-        }
-    })
-    .catch(err =>{
-        res.status(500).json({
-            error : err
-        })
-    });
+router.post('/signup', UsersController.users_signup);
 
-    
-});
+router.delete('/:userId', UsersController.user_delete);
 
-router.delete('/:userId', (req, res, next) =>{
-    User.remove({
-        _id : req.params.id
-    }).exec()
-    .then( result =>{
-        res.status(200).json({
-            message : 'User Deleted'
-        });
-    })
-    .catch(err =>{
-        res.status(500).json({
-            error : err
-        })
-    })
-
-});
-
-router.post('/login', (req,res,next) =>{
-    User.find({
-        email : req.body.email
-    }).exec()
-    .then( user =>{
-        if(user.length<1){
-            return res.status(404).json({
-                message : 'Auth Failed'
-            });
-        }
-        bcrypt.compare(req.body.password, user[0].password, (err, result)=>{
-            if(err){
-                return res.status(401).json({
-                    message : 'Auth failed'
-                });
-            }
-            if(result){
-                const token = jwt.sign({
-                    email : user[0].email,
-                    userId : user[0]._id
-                }, secret, 
-                {
-                    expiresIn : "1h"
-                }
-                )
-                return res.status(200).json({
-                    message : 'Auth Successful',
-                    token : token
-                });
-            }
-            return res.status(401).json({
-                message : 'Auth failed',
-            });
-
-        })
-    })
-    .catch();
-})
+router.post('/login', UsersController.user_login);
 
 module.exports = router
